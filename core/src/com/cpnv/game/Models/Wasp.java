355377b5bb1eb3d806d6baf08e2ea6d3ece98e5b
Quentin Neves <neves.quentin@gmail.com>
2019-05-16 15:57:05 +0200

    Make Wasp class usable

--- core/src/com/cpnv/game/Models/Wasp.java
+++ core/src/com/cpnv/game/Models/Wasp.java
@@ -1,12 +1,16 @@
 package com.cpnv.game.Models;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 
 import java.util.Random;
 
 public class Wasp extends MovingObject {
-    private static final String spritePath = "wasp.png";
+    private static final String spritePath = "bean.png";
     private static final Vector2 size = new Vector2(175, 150);
+
+    private static final int WORLD_WIDTH = 1600;
+    private static final int WORLD_HEIGHT = 900;
 
     public Wasp(Vector2 position) {
         super(spritePath, position, size);
@@ -14,14 +18,16 @@ public class Wasp extends MovingObject {
 
     @Override
     public void accelerate(float dt) {
-        int min = -10;
-        int max = 10;
+        int min = 0;
+        int max = 3;
 
         Random r = new Random();
-        int random = r.nextInt(max - min) + min;
+        int randomX = r.nextInt(max);
+        int randomY = r.nextInt(max);
 
-        velocity.x = random * dt * 1000;
-        velocity.y = random * dt * 1000;
+        velocity.x += (sprite.getX() > WORLD_WIDTH / 2) ? -randomX : randomX;
+        velocity.y += (sprite.getY() > WORLD_HEIGHT / 2) ? -randomY : randomY;
 
+        if (velocity.x + velocity.y > 40) { velocity.x /= 10; velocity.y /= 10; }
     }
 }
