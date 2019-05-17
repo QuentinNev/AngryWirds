4220e05f56191386d6b46dff28c54fa18eb79f51
Quentin Neves <neves.quentin@gmail.com>
2019-05-17 14:42:17 +0200

    Better wasp flight model

--- core/src/com/cpnv/game/Models/Wasp.java
+++ core/src/com/cpnv/game/Models/Wasp.java
@@ -11,6 +11,7 @@ public class Wasp extends MovingObject {
 
     private static final int WORLD_WIDTH = 1600;
     private static final int WORLD_HEIGHT = 900;
+    private static final int speedLimit = 10;
 
     public Wasp(Vector2 position) {
         super(spritePath, position, size);
@@ -19,15 +20,16 @@ public class Wasp extends MovingObject {
     @Override
     public void accelerate(float dt) {
         int min = 0;
-        int max = 3;
+        int max = 2;
 
         Random r = new Random();
         int randomX = r.nextInt(max);
         int randomY = r.nextInt(max);
 
         velocity.x += (sprite.getX() > WORLD_WIDTH / 2) ? -randomX : randomX;
-        velocity.y += (sprite.getY() > WORLD_HEIGHT / 2) ? -randomY : randomY;
+        velocity.y += (sprite.getY() > WORLD_HEIGHT / 2 + 100) ? -randomY : randomY;
 
-        if (velocity.x + velocity.y > 40) { velocity.x /= 10; velocity.y /= 10; }
+        velocity.x = velocity.x > speedLimit ? speedLimit : velocity.x;
+        velocity.y = velocity.y > speedLimit ? speedLimit : velocity.y;
     }
 }
