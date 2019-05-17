219454f90659ba7656480213b41eb5bc10b57b1a
Quentin Neves <neves.quentin@gmail.com>
2019-05-17 14:41:57 +0200

    Add method for adding objects with exception handling

--- core/src/com/cpnv/game/Models/Scenery.java
+++ core/src/com/cpnv/game/Models/Scenery.java
@@ -1,6 +1,7 @@
 package com.cpnv.game.Models;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
 
@@ -11,11 +12,21 @@ public final class Scenery {
         scene = new ArrayList<MovingObject>();
     }
 
-    public void add(MovingObject movingObject) {
-        scene.add(movingObject);
+    public void add(MovingObject object) {
+        scene.add(object);
     }
 
-    public void remove(MovingObject movingObject) { scene.remove(movingObject); }
+    public void addNonStackableObjects(MovingObject object) throws Exception  {
+        // Check before adding to the object if the space is free
+        Rectangle rectangle = object.getSprite().getBoundingRectangle();
+        for (MovingObject item: scene) {
+            if (rectangle.overlaps(item.getSprite().getBoundingRectangle())) throw new Exception("Something is already there");
+        }
+        scene.add(object);
+
+    }
+
+    public void remove(MovingObject object) { scene.remove(object); }
 
     public void draw(SpriteBatch batch) {
         for (MovingObject item: scene) {
