e9101e94bb58b24adf1aa38a988f115e0bca74ea
Quentin Neves <neves.quentin@gmail.com>
2019-06-06 14:32:59 +0200

    Set getLanguages for testing

--- core/src/com/cpnv/game/Models/VocabularyProvider.java
+++ core/src/com/cpnv/game/Models/VocabularyProvider.java
@@ -1,43 +1,74 @@
 package com.cpnv.game.Models;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Net;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+
+import org.omg.CORBA.Any;
 
 public class VocabularyProvider {
     private String vocUrlPrefix;
-    private Net.HttpRequest http;
+    protected Net.HttpRequest request;
+    private JsonReader json;
+    private JsonValue jsonValue;
 
-    public void create() {
-        http = new Net.HttpRequest(Net.HttpMethods.GET);
+    public VocabularyProvider() {
+        request = new Net.HttpRequest(Net.HttpMethods.GET);
         vocUrlPrefix = "http://voxerver.mycpnv.ch/api/v1/";
+        json = new JsonReader();
+    }
+
+    private void sendRequest() {
+        Gdx.app.log("ANGRY", "FUCK SHIT MAN");
+        Gdx.net.sendHttpRequest(request, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                String result = httpResponse.getResultAsString();
+                Gdx.app.log("ANGRY", result);
+
+            }
+
+            @Override
+            public void failed(Throwable throwable) {
+                Gdx.app.error("ANGRY", "Error",throwable);
+            }
+
+            @Override
+            public void cancelled() {
+
+            }
+        });
     }
 
     public void getLanguages() {
-        http.setUrl(vocUrlPrefix + "languages");
+        request.setUrl(vocUrlPrefix + "languages");
+        sendRequest();
 
     }
 
     public void getVoc(Integer vocId) {
-        http.setUrl(vocUrlPrefix + "voc/" + vocId);
+        request.setUrl(vocUrlPrefix + "voc/" + vocId);
 
     }
 
     public void getVocs() {
-        http.setUrl(vocUrlPrefix + "vocs");
+        request.setUrl(vocUrlPrefix + "vocs");
 
     }
 
     public void getVocs(Integer lang1, Integer lang2) {
-        http.setUrl(vocUrlPrefix + "vocs/" + lang1 + "/" +  lang2);
+        request.setUrl(vocUrlPrefix + "vocs/" + lang1 + "/" +  lang2);
 
     }
 
     public void getFullVocs() {
-        http.setUrl(vocUrlPrefix + "fullvocs");
+        request.setUrl(vocUrlPrefix + "fullvocs");
 
     }
 
     public void getAssignments(Integer studentToken) {
-        http.setUrl(vocUrlPrefix + "assignments" + studentToken);
+        request.setUrl(vocUrlPrefix + "assignments" + studentToken);
 
     }
 
