1591c7097df556ee0d8ab6285544a46ae3a7e18e
Quentin Neves <neves.quentin@gmail.com>
2019-06-13 15:12:10 +0200

    Fix bird movement

--- core/src/com/cpnv/game/Models/Bird.java
+++ core/src/com/cpnv/game/Models/Bird.java
@@ -1,35 +1,26 @@
 package com.cpnv.game.Models;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
-import java.util.Random;
-
 public class Bird extends MovingObject {
-    private float throwStrength;
     private static final Vector2 size = new Vector2(175, 150);
     private static final String spritePath = "bean.png";
     public boolean touched = false;
 
     public Bird(Vector2 position) {
         super(spritePath, position, size);
-        throwStrength = 0f;
         freezed = true;
     }
 
     @Override
     public void accelerate(float dt) {
-        if (!freezed) {
-            velocity.x = throwStrength * dt;
-            velocity.y += -g * dt * 5;
-        }
+        if (!freezed) velocity.y += -g * dt * 5;
     }
 
     @Override
     public void unFreeze() {
         freezed = false;
-        velocity.y = 10f;
     }
 
     @Override
@@ -45,8 +36,7 @@ public class Bird extends MovingObject {
     }
 
     public void setForce(Vector2 force){
-        throwStrength = force.x * 10;
-        velocity.y = force.y;
-        Gdx.app.log("ANGRY", "X : " + force.x * 10 + " // Y : " + force.y);
+        velocity.x = force.x / 10;
+        velocity.y = force.y / 20;
     }
 }
